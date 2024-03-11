7fc9fc59790a9557ecbf7e31fe6148269ff21d59
L1xus <omarstellar456@gmail.com>
2024-03-11 18:34:53 +0000

    AstroLib

---
+++ app/src/app/components/Footer.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+
+export default function Footer() {
+
+  return (
+    <footer className='bg-[#00668c] py-4 mt-auto rounded-t-[30px]'>
+      <div className='container mx-auto text-center'>
+        <p className='text-lg font-semibold text-[#fffefb] underline'>Swap $POX</p>
+      </div>
+      <div className='flex max-w-7xl mx-auto my-3'>
+        <div className='flex'>
+          <Image src="/fox.png" width={128} height={128} className='rounded-full'/>
+          <h1 className='text-xl font-semibold text-[#fe7e01] my-auto mx-2'>Astro_3262</h1>    
+        </div>
+        <div className='flex my-auto ml-auto'>
+          <div style={{ width: '30px', height: '30px', margin: 'auto 10px' }}>
+            <Image src='/x.svg' width={30} height={30} />
+          </div>
+           <div style={{ width: '40px', height: '40px' }}>
+            <Image src='/github.svg' width={40} height={40} />
+          </div>
+        </div>
+     </div>
+    </footer> 
+  )
+}
--- app/src/app/components/Library.tsx
+++ app/src/app/components/Library.tsx
@@ -11,34 +11,38 @@ export default function Library() {
         <h1 className='text-xl font-semibold ps-1 uppercase text-[#3b3c3d]'>Library</h1>
       </div>
       <div className='mt-3'>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
-
-          <div className='flex bg-red-600 h-48 p-3'>
-            <div className='w-1/2 h-full mr-3'>
-              <img src="/test.jpg" alt="" className="h-full object-cover" loading="lazy" />
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+          {[...Array(4)].map((_, idx) => (
+            <div key={idx} className='flex bg-[#b6ccd8] shadow-xl h-48 p-3 rounded'>
+              <div className='w-auto h-full'>
+                <img src="/test.jpg" alt="" className="h-full object-cover rounded" loading="lazy" />
+              </div>
+              <div className='flex-grow my-auto px-5'>
+                <div className='flex items-center'>
+                  <div style={{ width: '16px', height: '16px' }}>
+                    <Image src='/book.png' width={16} height={16} />
+                  </div>
+                  <h1 className='text-lg font-semibold text-[#00668c] pl-1 uppercase'>Book Title N1</h1>
+                </div>
+                <div className='flex my-3'>
+                  <h1 className='text-base font-medium text-[#313d44] mr-auto'>Shahoo Yousofi</h1>
+                  {[...Array(5)].map((_, idx) => (
+                    <div key={idx} style={{ width: '16px', height: '16px' }}>
+                      <Image src='/star.png' width={16} height={16} />
+                    </div>
+                  ))}
+                </div>
+                <div className='h-px w-full bg-[#00668c]'></div>
+                <p className='text-sm text-[#313d44] mt-3 mb-1'>Price</p>
+                <div className='flex'>
+                  <div style={{ width: '28px', height: '28px' }} className='rounded-full'>
+                    <Image src='/pox.png' width={28} height={28} />
+                  </div>
+                  <p className='text-lg font-semibold text-[#00668c] ml-2'>1300</p> 
+                </div>
+              </div>
             </div>
-            <div className='w-1/2'>
-              <h1>Book Title N1</h1>
-            </div>
-          </div>
-
-          <div className='bg-red-600 h-48'>
-            <div className='w-full h-full'>
-              <img src="/test.jpg" alt="" className="h-full object-cover" loading="lazy" />
-            </div>
-          </div>
-
-           <div className='bg-red-600 h-48'>
-            <div className='w-full h-full'>
-              <img src="/test.jpg" alt="" className="h-full object-cover" loading="lazy" />
-            </div>
-          </div>
-
-           <div className='bg-red-600 h-48'>
-            <div className='w-full h-full'>
-              <img src="/test.jpg" alt="" className="h-full object-cover" loading="lazy" />
-            </div>
-          </div>
+          ))}
 
         </div>
       </div>
